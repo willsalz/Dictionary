c3970435147199fb381db2e7af39f3280df3c234
Will Salisbury <wsalisbury@apple.com>
2014-05-24 16:05:22 -0700

    Complete string->string dictionary implementation.

---
+++ Dictionary/dictionary.c
@@ -0,0 +1,9 @@
+//
+//  dictionary.c
+//  Dictionary
+//
+//  Created by Will Salisbury on 5/24/14.
+//  Copyright (c) 2014 Pattern, LLC. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ dictionary.h
@@ -0,0 +1,14 @@
+//
+//  dictionary.h
+//  Dictionary
+//
+//  Created by Will Salisbury on 5/24/14.
+//  Copyright (c) 2014 Pattern, LLC. All rights reserved.
+//
+
+#ifndef Dictionary_dictionary_h
+#define Dictionary_dictionary_h
+
+
+
+#endif
