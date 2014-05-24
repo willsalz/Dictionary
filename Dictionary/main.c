91f29a3e1809552e2528500e077fcc3453d8dfcf
Will Salisbury <wsalisbury@apple.com>
2014-05-24 12:40:45 -0700

    Initial Commit

---
+++ Dictionary/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Dictionary
+//
+//  Created by Will Salisbury on 5/24/14.
+//  Copyright (c) 2014 Pattern, LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
